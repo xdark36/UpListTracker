4ce1e6b703854a071e7e37d2ad9097e4ef009280
xdark36 <noahdeems@gmail.com>
2025-06-29 23:42:52 -0400

    Apply latest drop-in gradle & App.kt changes

---
+++ app/src/main/java/com/example/uplisttracker/App.kt
@@ -0,0 +1,21 @@
+// File: app/src/main/java/com/example/uplisttracker/App.kt
+
+package com.example.uplisttracker
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
